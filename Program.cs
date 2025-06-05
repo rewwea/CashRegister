3498c5fcaf3fff3d52a6b10384c70244e06624b4
Artem Andronov <AVAndronov10@gmail.com>
2025-06-05 11:01:51 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите нижнюю границу диапазона: ");
+        int min = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите верхнюю границу диапазона: ");
+        int max = int.Parse(Console.ReadLine());
+
+        int number;
+        do
+        {
+            Console.Write("Введите число в заданном диапазоне: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number < min || number > max)
+            {
+                Console.WriteLine("Число вне диапазона. Попробуйте снова.");
+            }
+        } while (number < min || number > max);
+
+        Console.WriteLine($"Число {number} находится в диапазоне [{min}, {max}]");
+    }
+}
